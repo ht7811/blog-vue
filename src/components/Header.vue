a6ec61a8c14a95650576ce760e34864e4db80175
ht7811 <302442731@qq.com>
2019-03-13 18:43:47 +0800

    Signed-off-by: ht7811 <302442731@qq.com>

---
+++ src/components/Header.vue
@@ -0,0 +1,513 @@
+<template>
+    <header id="header">			
+        <h1><a href="#">咦，我怎么在这里</a></h1>
+        <nav class="links">
+            <ul>
+                <li><a href="https://www.heting2gjy.com:443/index.jsp">Home</a></li>
+                <li><a href="#">Vue.js</a></li>
+                <li><a href="#">Java</a></li>
+                <li><a href="#">NodeJs</a></li>
+                <li><a href="#">Other</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+        <nav class="main">
+            <ul>
+                <li class="search">
+                    <a class="fa-search" href="#search">Search</a>
+                    <form id="search" method="get" action="#">
+                        <input type="text" name="query" placeholder="Search">
+                    </form>
+                </li>
+                <li class="menu">
+                    <a class="fa-bars" href="#menu">Menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        
+    }
+}
+</script>
+
+<style>
+    /* Reset */
+
+	html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
+		font-family: "Source Han Sans CN"
+	}
+
+	article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
+		display: block;
+	}
+
+	body {
+		line-height: 1;
+	}
+
+	ol, ul {
+		list-style: none;
+	}
+
+	blockquote, q {
+		quotes: none;
+	}
+
+	blockquote:before, blockquote:after, q:before, q:after {
+		content: '';
+		content: none;
+	}
+
+	table {
+		border-collapse: collapse;
+		border-spacing: 0;
+	}
+
+	body {
+		-webkit-text-size-adjust: none;
+	}
+	input:-webkit-autofill {
+    	-webkit-box-shadow: 0 0 0px 1000px white inset !important;
+	}
+
+    /* Box Model */
+
+	*, *:before, *:after {
+		-moz-box-sizing: border-box;
+		-webkit-box-sizing: border-box;
+		box-sizing: border-box;
+	}
+	.clearfix:before,.clearfix:after{content:"";display:table}
+	.clearfix:after{clear:both}
+    .copyrights{
+        text-indent:-9999px;
+        height:0;
+        line-height:0;
+        font-size:0;
+        overflow:hidden;
+    }
+
+    /* Basic */
+
+	@-ms-viewport {
+		width: device-width;
+	}
+
+	body {
+		-ms-overflow-style: scrollbar;
+	}
+
+	@media screen and (max-width: 480px) {
+
+		html, body {
+			min-width: 320px;
+		}
+
+	}
+
+	body {
+		background: #f4f4f4;
+	}
+
+    body.is-loading *, body.is-loading *:before, body.is-loading *:after {
+        -moz-animation: none !important;
+        -webkit-animation: none !important;
+        -ms-animation: none !important;
+        animation: none !important;
+        -moz-transition: none !important;
+        -webkit-transition: none !important;
+        -ms-transition: none !important;
+        transition: none !important;
+    }
+
+    /* Type */
+
+	body, input, select, textarea {
+		color: #646464;
+		font-family: "Source Sans Pro", Helvetica, sans-serif;
+		font-size: 14pt;
+		font-weight: 400;
+		line-height: 1.75;
+	}
+
+    @media screen and (max-width: 1680px) {
+
+        body, input, select, textarea {
+            font-size: 12pt;
+        }
+
+    }
+
+    @media screen and (max-width: 1280px) {
+
+        body, input, select, textarea {
+            font-size: 12pt;
+        }
+
+    }
+
+    @media screen and (max-width: 980px) {
+
+        body, input, select, textarea {
+            font-size: 12pt;
+        }
+
+    }
+
+    @media screen and (max-width: 736px) {
+
+        body, input, select, textarea {
+            font-size: 12pt;
+        }
+
+    }
+
+    @media screen and (max-width: 480px) {
+
+        body, input, select, textarea {
+            font-size: 12pt;
+        }
+
+    }
+
+	a {
+		-moz-transition: color 0.2s ease, border-bottom-color 0.2s ease;
+		-webkit-transition: color 0.2s ease, border-bottom-color 0.2s ease;
+		-ms-transition: color 0.2s ease, border-bottom-color 0.2s ease;
+		transition: color 0.2s ease, border-bottom-color 0.2s ease;
+		border-bottom: dotted 1px rgba(160, 160, 160, 0.65);
+		color: inherit;
+		text-decoration: none;
+	}
+
+    a:before {
+        -moz-transition: color 0.2s ease;
+        -webkit-transition: color 0.2s ease;
+        -ms-transition: color 0.2s ease;
+        transition: color 0.2s ease;
+    }
+
+    a:hover {
+        border-bottom-color: transparent;
+        color: #2ebaae !important;
+    }
+
+    a:hover:before {
+        color: #2ebaae !important;
+    }
+
+	strong, b {
+		color: #3c3b3b;
+		font-weight: 700;
+	}
+
+	em, i {
+		font-style: italic;
+	}
+
+	p {
+		margin: 0 0 2em 0;
+	}
+
+	h1, h2, h3, h4, h5, h6 {
+		color: #3c3b3b;
+		font-family: "Raleway", Helvetica, sans-serif;
+		font-weight: 800;
+		letter-spacing: 0.25em;
+		line-height: 1.65;
+		margin: 0 0 1em 0;
+		text-transform: uppercase;
+	}
+
+    h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {
+        color: inherit;
+        border-bottom: 0;
+    }
+
+	h2 {
+		font-size: 1.1em;
+	}
+
+	h3 {
+		font-size: 0.9em;
+	}
+
+	h4 {
+		font-size: 0.7em;
+	}
+
+	h5 {
+		font-size: 0.7em;
+	}
+
+	h6 {
+		font-size: 0.7em;
+	}
+
+	sub {
+		font-size: 0.8em;
+		position: relative;
+		top: 0.5em;
+	}
+
+	sup {
+		font-size: 0.8em;
+		position: relative;
+		top: -0.5em;
+	}
+
+	blockquote {
+		border-left: solid 4px rgba(160, 160, 160, 0.3);
+		font-style: italic;
+		margin: 0 0 2em 0;
+		padding: 0.5em 0 0.5em 2em;
+	}
+
+	code {
+		background: rgba(160, 160, 160, 0.075);
+		border: solid 1px rgba(160, 160, 160, 0.3);
+		font-family: "Courier New", monospace;
+		font-size: 0.9em;
+		margin: 0 0.25em;
+		padding: 0.25em 0.65em;
+	}
+
+	pre {
+		-webkit-overflow-scrolling: touch;
+		font-family: "Courier New", monospace;
+		font-size: 0.9em;
+		margin: 0 0 2em 0;
+	}
+
+    pre code {
+        display: block;
+        line-height: 1.75em;
+        padding: 1em 1.5em;
+        overflow-x: auto;
+    }
+
+	hr {
+		border: 0;
+		border-bottom: solid 1px rgba(160, 160, 160, 0.3);
+		margin: 2em 0;
+	}
+
+    hr.major {
+        margin: 3em 0;
+    }
+
+	.align-left {
+		text-align: left;
+	}
+
+	.align-center {
+		text-align: center;
+	}
+
+	.align-right {
+		text-align: right;
+	}
+
+    #header {
+		display: -moz-flex;
+		display: -webkit-flex;
+		display: -ms-flex;
+		display: flex;
+		-moz-justify-content: space-between;
+		-webkit-justify-content: space-between;
+		-ms-justify-content: space-between;
+		justify-content: space-between;
+		background-color: #ffffff;
+		border-bottom: solid 1px rgba(160, 160, 160, 0.3);
+		height: 3.5em;
+		left: 0;
+		line-height: 3.5em;
+		position: fixed;
+		top: 0;
+		width: 100%;
+		z-index: 10000;
+	}
+
+    #header a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    #header ul {
+        list-style: none;
+        margin: 0;
+        padding-left: 0;
+        text-align: left;
+    }
+
+    #header ul li {
+        display: inline-block;
+        padding-left: 0;
+    }
+
+    #header h1 {
+        height: inherit;
+        line-height: inherit;
+        padding: 0 0 0 1.5em;
+        white-space: nowrap;
+    }
+
+    #header h1 a {
+        font-size: 0.7em;
+    }
+
+    #header .links {
+        -moz-flex: 1;
+        -webkit-flex: 1;
+        -ms-flex: 1;
+        flex: 1;
+        border-left: solid 1px rgba(160, 160, 160, 0.3);
+        height: inherit;
+        line-height: inherit;
+        margin-left: 1.5em;
+        overflow: hidden;
+        padding-left: 1.5em;
+    }
+
+    #header .links ul li {
+        border-left: solid 1px rgba(160, 160, 160, 0.3);
+        line-height: 1;
+        margin-left: 1em;
+        padding-left: 1em;
+    }
+
+    #header .links ul li:first-child {
+        border-left: 0;
+        margin-left: 0;
+        padding-left: 0;
+    }
+
+    #header .links ul li a {
+        border-bottom: 0;
+        font-family: "Raleway", Helvetica, sans-serif;
+        font-size: 0.7em;
+        font-weight: 400;
+        letter-spacing: 0.25em;
+        text-transform: uppercase;
+    }
+
+    #header .main {
+        height: inherit;
+        line-height: inherit;
+        text-align: right;
+    }
+
+    #header .main ul {
+        height: inherit;
+        line-height: inherit;
+    }
+
+    #header .main ul li {
+        border-left: solid 1px rgba(160, 160, 160, 0.3);
+        height: inherit;
+        line-height: inherit;
+        white-space: nowrap;
+    }
+
+    #header .main ul li > * {
+        display: block;
+        float: left;
+    }
+
+    #header .main ul li > a {
+        text-decoration: none;
+        border-bottom: 0;
+        color: #aaaaaa;
+        overflow: hidden;
+        position: relative;
+        text-indent: 4em;
+        width: 4em;
+    }
+
+    #header .main ul li > a:before {
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        font-family: FontAwesome;
+        font-style: normal;
+        font-weight: normal;
+        text-transform: none !important;
+    }
+
+    #header .main ul li > a:before {
+        display: block;
+        height: inherit;
+        left: 0;
+        line-height: inherit;
+        position: absolute;
+        text-align: center;
+        text-indent: 0;
+        top: 0;
+        width: inherit;
+    }
+
+    #header form {
+        margin: 0;
+    }
+
+    #header form input {
+        display: inline-block;
+        height: 2.5em;
+        position: relative;
+        top: -2px;
+        vertical-align: middle;
+    }
+
+    #header #search {
+        -moz-transition: all 0.5s ease;
+        -webkit-transition: all 0.5s ease;
+        -ms-transition: all 0.5s ease;
+        transition: all 0.5s ease;
+        max-width: 0;
+        opacity: 0;
+        overflow: hidden;
+        padding: 0;
+        white-space: nowrap;
+    }
+
+    #header #search input {
+        width: 12em;
+    }
+
+    #header #search.visible {
+        max-width: 12.5em;
+        opacity: 1;
+        padding: 0 0.5em 0 0;
+    }
+
+    @media screen and (max-width: 980px) {
+        #header .links {
+            display: none;
+        }
+    }
+
+    @media screen and (max-width: 736px) {
+        #header {
+            height: 2.75em;
+            line-height: 2.75em;
+        }
+        #header h1 {
+            padding: 0 0 0 1em;
+        }
+        #header .main .search {
+            display: none;
+        }
+
+    }
+</style>
+
---
+++ src/views/Main.vue
@@ -0,0 +1,180 @@
+<template>
+<div id="wrapper">
+    <Header></Header>
+    <div id="main">
+        <article class="post">
+            <header>
+                <div class="title">
+                    <h2><a href="#">echarts学习(3)</a></h2>
+                    <p>云服务器上JavaWeb项目运行环境需要安装些什么？</p>
+                </div>
+                <div class="meta">
+                    <time class="published" datetime="2018-05-16">2018-05-25 09:58:18.0</time>
+                    <a href="#" class="author"><span class="name">咦，我怎么在这里</span><img src="https://www.heting2gjy.com:443/static/images/avatar.jpg" alt=""></a>
+                </div>
+            </header>
+            
+            <p>echarts学习(3)</p>
+            <footer>
+                <ul class="actions">
+                    <li><a href="https://www.heting2gjy.com:443/details.jsp?id=f9f994b4-45d5-4b24-8d68-b2cb14898505" class="button big">继续阅读</a></li>
+                </ul>
+                <ul class="stats">
+                    <li><a href="#">General</a></li>
+                    <li><a href="#" class="icon fa-heart">28</a></li>
+                    <li><a href="#" class="icon fa-comment">128</a></li>
+                </ul>
+            </footer>
+        </article>
+    </div>
+    <section id="sidebar">
+        <!-- Intro -->
+        <section id="intro">
+            <a href="#" class="logo"><img src="https://www.heting2gjy.com:443/static/images/logo.jpg" alt=""></a>
+            <header>
+                <h2>游客</h2>
+                <p>职业：</p>
+            </header>
+        </section>
+
+        <!-- Mini Posts -->
+        <section>
+            <div class="mini-posts">
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="#">Vue.js</a></h3>
+                        <time class="published" datetime="2018-05-20">05 20, 2018</time>
+                        <a href="#" class="author"><img src="https://www.heting2gjy.com:443/static/images/avatar.jpg" alt=""></a>
+                    </header>
+                    <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic04.jpg" alt=""></a>
+                </article>
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="#">Vue.js</a></h3>
+                        <time class="published" datetime="2018-05-19">05 19, 2018</time>
+                        <a href="#" class="author"><img src="https://www.heting2gjy.com:443/static/images/avatar.jpg" alt=""></a>
+                    </header>
+                    <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic05.jpg" alt=""></a>
+                </article>
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="#">Vue.js</a></h3>
+                        <time class="published" datetime="2018-05-18">05 18, 2018</time>
+                        <a href="#" class="author"><img src="https://www.heting2gjy.com:443/static/images/avatar.jpg" alt=""></a>
+                    </header>
+                    <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic06.jpg" alt=""></a>
+                </article>
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="#">Vue.js</a></h3>
+                        <time class="published" datetime="2018-05-17">05 17, 2018</time>
+                        <a href="#" class="author"><img src="https://www.heting2gjy.com:443/static/images/avatar.jpg" alt=""></a>
+                    </header>
+                    <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic07.jpg" alt=""></a>
+                </article>
+
+            </div>
+        </section>
+
+        <!-- Posts List -->
+        <section>
+            <ul class="posts">
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="#">Vue.js</a></h3>
+                            <time class="published" datetime="2018-05-20">05 20, 2018</time>
+                        </header>
+                        <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic08.jpg" alt=""></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="#">Vue.js</a></h3>
+                            <time class="published" datetime="2018-05-15">05 15, 2018</time>
+                        </header>
+                        <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic09.jpg" alt=""></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="#">Vue.js</a></h3>
+                            <time class="published" datetime="2018-05-10">05 10, 2018</time>
+                        </header>
+                        <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic10.jpg" alt=""></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="#">Vue.js</a></h3>
+                            <time class="published" datetime="2018-05-08">05 8, 2018</time>
+                        </header>
+                        <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic11.jpg" alt=""></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="#">Vue.js</a></h3>
+                            <time class="published" datetime="2018-05-06">05 7, 2018</time>
+                        </header>
+                        <a href="#" class="image"><img src="https://www.heting2gjy.com:443/static/images/pic12.jpg" alt=""></a>
+                    </article>
+                </li>
+            </ul>
+        </section>
+
+        <!-- About -->
+        <section class="blurb">
+            <h2>About</h2>
+            <p>闲着没事瞎做的。。。</p>
+            <ul class="actions">
+                <li><a href="#" class="button">了解更多</a></li>
+            </ul>
+        </section>
+
+        <!-- Footer -->
+        <section id="footer">
+            <ul class="icons">
+                <li><a title="微信" class="fa-weixin"><span class="label">微信</span></a></li>
+                <li><a title="微博" class="fa-weibo"><span class="label">微博</span></a></li>
+                <li><a title="电话" class="fa-whatsapp"><span class="label">电话</span></a></li>
+                <li><a title="30242731" class="fa-qq"><span class="label">QQ</span></a></li>
+                <li><a title="302442731@qq.com" class="fa-envelope"><span class="label">Email</span></a></li>
+            </ul>
+        </section>
+
+    </section>
+  </div>
+</template>
+
+<script>
+    import Header from '@/components/Header';
+    export default {
+        name: 'Main',
+        components: {
+            Header
+        },
+        data () {
+            return {
+            msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
